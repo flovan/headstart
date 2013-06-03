61efa3d2ca09cef91908371aae2e90bd025434c0
Gwen Vanhee <gwen_vanhee@hotmail.com>
2013-06-03 18:05:15 +0200

    Updated filenames to prevent Github Mac App from crashing

--- js/sources/app{{__DEFAULT__}}.js
+++ js/sources/app{{__DEFAULT__}}.js
@@ -10,10 +10,14 @@ $(document).ready(function()
 	// Important: __utils.js sets up a global namespace for all utilities
 	// Add this snippet before adding any other utilities
 	//
+	// If Underscore/Lo-Dash is included in your project
+	// you can use _.debounce(callback, wait) instead
+	//
 
 	// @codekit-prepend "__utils.js"
 	// @codekit-prepend "__utils.clientdetection.js"
 	// @codekit-prepend "__utils.cookies.js"
+	// @codekit-prepend "__utils.debounce.js"
 
 
 
