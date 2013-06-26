7de9e409e1020e172c9e89ff4fb22e30da5bcfd3
Gwen Vanhee <gwen_vanhee@hotmail.com>
2013-06-26 18:38:13 +0200

    Updated type check

--- js/sources/__utils.strings.capitalize.js
+++ js/sources/__utils.strings.capitalize.js
@@ -7,8 +7,8 @@
 
 Utils.capitalizeString = function(string, type)
 {
-	if(typeof type === 'string' && type === 'each-word') return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
-	if(typeof type === 'string' && type === 'all-chars') return string.toUpperCase();
+	if(type && type === 'each-word') return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+	if(type && type === 'all-chars') return string.toUpperCase();
 
 	return string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
 };
