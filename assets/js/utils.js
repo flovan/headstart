c8df910aa7566db2ce805c7bd69b7fdf7ccc754d
Florian <accounts@flovan.me>
2013-09-03 09:31:37 +0200

    Included better function to add console.log support to IE

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -5,24 +5,13 @@
 // Avoid `console` errors in browsers that lack a console. */
 
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+    if(Function.prototype.bind && window.console && typeof console.log == "object")
+    {
+    	["log","info","warn","error","assert","dir","clear","profile","profileEnd"
+        ].forEach(function (method)
+        {
+            console[method] = this.bind(console[method], console);
+        }, Function.prototype.call);
     }
 }());
 
