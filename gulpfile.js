e0b89f5454b88b683147e43aaa7f85b9def209b8
Florian <accounts@flovan.me>
2014-01-28 22:53:42 +0100

    Update to Gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,28 @@ var gulp 			= require('gulp'),
     sass 			= require('gulp-ruby-sass'),
     autoprefixer 	= require('gulp-autoprefixer'),
     minifycss 		= require('gulp-minify-css'),
-    jshint 			= require('gulp-jshint'),
     uglify 			= require('gulp-uglify'),
     imagemin 		= require('gulp-imagemin'),
     rename 			= require('gulp-rename'),
-    clean 			= require('gulp-clean'),
+    rimraf 			= require('gulp-rimraf'),
     concat 			= require('gulp-concat'),
-    notify 			= require('gulp-notify'),
     cache 			= require('gulp-cache'),
     livereload 		= require('gulp-livereload'),
     lr 				= require('tiny-lr'),
     server 			= lr();
+
+// Clean up -------------------------------------------------------------------
+//
+// This task does the following things:
+// - Clean out the dist folder before rebuilding
+
+gulp.task('clean', function(cb)
+{
+	gulp.src(['dist'], {read: false})
+		.pipe(rimraf());
+
+	cb();
+});
 
 // Styles ---------------------------------------------------------------------
 //
@@ -29,9 +40,9 @@ var gulp 			= require('gulp'),
 // - Save and minify results to .css files
 // - Reload browser
 
-gulp.task('styles', function()
+gulp.task('styles', function(cb)
 {
-	return gulp.src('assets/sass/*.scss')
+	gulp.src('assets/sass/*.scss')
 		.pipe(plumber())
 		.pipe(sass({ style: 'nested', compass: true }))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
@@ -39,8 +50,9 @@ gulp.task('styles', function()
 		.pipe(rename({suffix: '.min'}))
 		.pipe(minifycss())
 		.pipe(gulp.dest('dist/assets/css'))
-		.pipe(livereload(server))
-		.pipe(notify({ message: 'Styles task complete' }));
+		.pipe(livereload(server));
+
+	cb();
 });
 
 // Scripts --------------------------------------------------------------------
@@ -50,23 +62,23 @@ gulp.task('styles', function()
 // - Uglify, and save again as .min
 // - Reload browser
 
-gulp.task('scripts', function()
+gulp.task('scripts', function(cb)
 {
-	gulp.src(['assets/js/libs/*.js', '!**/jquery*.js', 'assets/js/core/*.js', 'assets/js/app.js'])
+	// Concat jQuery in front of libs, core files and the app
+	gulp.src(['assets/js/libs/jquery*.js', 'assets/js/libs/*.js', '!assets/js/libs/jquery*.js', 'assets/js/core/*.js', 'assets/js/app.js'])
 		.pipe(plumber())
-		.pipe(concat('core-libs.tmp.js'))
-		.pipe(gulp.dest('dist/tmp'));
-
-	return gulp.src(['assets/js/libs/jquery*.js', 'dist/tmp/core-libs.tmp.js'])
-		.pipe(plumber())
-		//.pipe(jshint('.jshintrc'))
-		//.pipe(jshint.reporter('default'))
-		.pipe(concat('core-libs.js'))
-		.pipe(rename({suffix: '.min'}))
+		.pipe(concat('core-libs.min.js'))
 		//.pipe(uglify())
-		.pipe(gulp.dest('dist/assets/js'))
-		.pipe(livereload(server))
-		.pipe(notify({ message: 'Scripts task complete' }));
+		.pipe(gulp.dest('dist/assets/js'));
+
+	// Minify view specific files
+	gulp.src('assets/js/view/*.js')
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/assets/js/view'))
+		.pipe(livereload(server));
+
+	cb();
 });
 
 // IE -------------------------------------------------------------------------
@@ -74,16 +86,16 @@ gulp.task('scripts', function()
 // Same as scripts, but targeted for IE
 // Note: Order is important!!!
 
-gulp.task('ie-scripts', function()
+gulp.task('ie-scripts', function(cb)
 {
-	return gulp.src(['**/html5shiv.js', '**/nwmatcher-1.2.5.js', '**/selectivizr.js', '**/matchmedia.polyfill.js', '**/matchmedia.addListener.js', '**/respond.js'])
+	gulp.src(['**/html5shiv.js', '**/nwmatcher-1.2.5.js', '**/selectivizr.js', '**/matchmedia.polyfill.js', '**/matchmedia.addListener.js', '**/respond.js'])
 		.pipe(plumber())
-		.pipe(concat('ie.js'))
-		.pipe(rename({suffix: '.min'}))
+		.pipe(concat('ie.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('dist/assets/js'))
-		.pipe(livereload(server))
-		.pipe(notify({ message: 'IE Scripts task complete' }));
+		.pipe(livereload(server));
+
+	cb();
 });
 
 // Images ---------------------------------------------------------------------
@@ -93,47 +105,28 @@ gulp.task('ie-scripts', function()
 // - Optimize them
 // - Reload browser
 
-gulp.task('images', function()
+gulp.task('images', function(cb)
 {
-	return gulp.src('assets/images/**/*')
+	gulp.src('assets/images/**/*')
 		.pipe(plumber())
 		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
 		.pipe(gulp.dest('dist/assets/images'))
-		.pipe(livereload(server))
-		.pipe(notify({ message: 'Images task complete' }));
-});
+		.pipe(livereload(server));
 
-// Clean up -------------------------------------------------------------------
-//
-// This task does the following things:
-// - Clean out the dist folder before rebuilding
-
-gulp.task('clean', function()
-{
-	return gulp.src(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], {read: false})
-		.pipe(clean());
-});
-
-// The default task -----------------------------------------------------------
-//
-// Runs tasks in order
-// On end clean tmp files that were generated
-
-gulp.task('default', ['clean', 'styles', 'scripts', 'ie-scripts', 'images', 'watch'], function()
-{
-	/*gulp.src('dist/tmp', {read: false})
-		.pipe(clean());*/
+	cb();
 });
 
 // Watch ----------------------------------------------------------------------
 //
 // Watch files for changes and run appropriate task
 
-gulp.task('watch', function()
+gulp.task('watch', function(cb)
 {
 	server.listen(35729, function(err)
 	{
 		if (err) { return console.log(err) };
+
+		console.log('Started watching assets folder..')
 
 		gulp.watch('assets/sass/*.scss', ['styles'], function(event)
 		{
@@ -150,6 +143,21 @@ gulp.task('watch', function()
       		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
       	});
 	});
+
+	cb();
+});
+
+// The default task -----------------------------------------------------------
+//
+// Runs tasks in order when you call 'gulp' from the CLI
+
+// TODO:
+// Use dependencies instead of gulp.run()
+// Not as easy as it seems though
+
+gulp.task('default', ['clean'], function()
+{
+	gulp.run('styles', 'scripts', 'ie-scripts', /*'images',*/ 'watch');
 });
 
 
