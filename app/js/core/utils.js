4f46ba054ed981f3b5586f5e0998407300331581
winteregel <florian@adagiomobile.com>
2014-01-29 22:43:14 +0100

    Fixed small strict bugs

--- app/js/core/utils.js
+++ app/js/core/utils.js
@@ -7,16 +7,6 @@ var Utils = function()
 	var isTouch			= null;
 
 	return {
-	
-		// Browser sniffing
-		
-		// Don't use this >:(
-		// 		detectUserAgent: function()
-		// 		{
-		// 			var b = document.documentElement;
-		// 		  	b.setAttribute('data-useragent',  navigator.userAgent);
-		// 		  	b.setAttribute('data-platform', navigator.platform );
-		// 		},
 
 		// Check for touch event support
 
@@ -24,64 +14,6 @@ var Utils = function()
 		{
 			if(_.isNull(isTouch)) isTouch = ('ontouchstart' in window) || (window.DocumentTouch && document instanceof DocumentTouch);
 			return isTouch;
-		},
-		
-		// Debounce-able resize event
-		
-		resize: function(callback, wait)
-		{
-			if(!_.isNumber(wait)) wait = 75;
-			
-		    function resizeFct()
-		    {
-		        var width  = document.documentElement.clientWidth,
-		            height = document.documentElement.clientHeight;
-		
-		        callback(width, height);
-		    }
-		
-		    if(window.attachEvent) window.attachEvent('onresize', _.debounce(resizeFct, wait));
-		    if(window.addEventListener) window.addEventListener('resize', _.debounce(resizeFct, wait));
-		    if(window.orientationchange) window.addEventListener('orientationchange', _.debounce(resizeFct, wait));
-		
-		    resizeFct();
-		},
-		
-		// Debounce-able scroll event
-		
-		scroll: function(callback, wait)
-		{
-			if(!_.isNumber(wait)) wait = 75;
-			
-		    function scrollFct()
-		    {
-		        var width  		= document.documentElement.clientWidth,
-		            height 		= document.documentElement.clientHeight;
-		        
-		        var body 		= document.body, //IE 'quirks'
-		            doc 		= document.documentElement, //IE with doctype
-		            doc 		= (doc.clientHeight)? doc : body,
-		            scrollTop 	= doc.scrollTop;
-		
-		        callback(width, height, scrollTop);
-		    }
-		
-			// TODO:
-			// Check if Android devices do a better job at dispatching scroll events than iOS devices.
-		    
-		    if(window.attachEvent)
-		    {
-		    	if(!Utils.isTouch()) window.attachEvent('onscroll', _.throttle(scrollFct, wait));
-		    	else window.attachEvent('ontouchmove', _.throttle(scrollFct, wait));
-		    }
-		    
-		    if(window.addEventListener)
-		    {
-		    	if(!Utils.isTouch()) document.addEventListener('scroll', _.throttle(scrollFct, wait));
-		    	else document.addEventListener('touchmove', _.throttle(scrollFct, wait), false);
-		    }	
-		    
-		    scrollFct();
 		},
 
 		// Function for inserting report messages
@@ -119,7 +51,7 @@ var Utils = function()
 				});
 			}
 		}
-	}
+	};
 }();
 
 // $form.serializeObject() ----------------------------------------------------
