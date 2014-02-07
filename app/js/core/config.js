e8a49db3715c069ddd28e28079584047192b83a4
Florian <accounts@flovan.me>
2014-02-07 20:15:12 +0100

    Removed redundant code

--- app/js/core/config.js
+++ app/js/core/config.js
@@ -1,8 +1,6 @@
 var Config = function()
 {
 	return {
-		// An easy flag to turn of logging completely
-		devFlag:			false,
 		// Number of ms to wait before the API cancels a call
 		// Only applies to "cancellable" calls 
 		apiHangWait:		5000,
