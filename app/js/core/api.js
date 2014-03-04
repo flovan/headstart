9b379c0067eb3b8779bfc4a9e76da899591f49dd
Florian <accounts@flovan.me>
2014-03-04 16:52:16 +0100

    Cleaned up
    Moved to single line opening brackets, dropped comma-first approach,
    basic JSDocs added

--- app/js/core/api.js
+++ app/js/core/api.js
@@ -4,35 +4,32 @@
 /* global _: false */
 /* global App: false */
 
-// API class ------------------------------------------------------------------
-//
-// A wrapper class for making $.ajax() calls
+/** @namespace A wrapper class for making Ajax calls */
+var Api = function() {
 
-var Api = function()
-{
 	'use strict';
 	
 	// PRIVATE FUNCTIONS ------------------------------------------------------
 	
-	function _ajax(options)
-	{
+	function _ajax(options) {
+
 		var req,
 			hangTO,
 			cancelTO;
 		
 		// If cancellable set timouts
 		// configureable through config.js
-		if(options.cancellable)
-		{
-			hangTO = setTimeout(function()
-			{
+		if(options.cancellable) {
+
+			hangTO = setTimeout(function() {
+
 				// Let observers know the call is hanging
 				App.dispatcher.trigger(EVENTS.api.apiHang, { route: options.route });
 				
 			}, App.config.serviceHangWait);
 			
-			cancelTO = setTimeout(function()
-			{
+			cancelTO = setTimeout(function() {
+
 				// Abort the call
 				if(req) req.abort();
 				// Let observers know the call was cancelled
@@ -42,13 +39,12 @@ var Api = function()
 		}
 		
 		// make the ajax call
-		req = $.ajax(
-		{
+		req = $.ajax({
 			type:	options.method,
 			url:	options.route,
 			data:	options.values,
-		}).done(function(data)
-		{
+		}).done(function(data) {
+
 			// Clear timeouts if there are any
 			if(hangTO) clearTimeout(hangTO);
 			if(cancelTO) clearTimeout(cancelTO);
@@ -58,8 +54,8 @@ var Api = function()
 			if(_.isFunction(options.done)) options.done(data);
 			else log('Route ' + options.route + ' received a result, but no callback was supplied', data);
 			
-		}).fail(function(data)
-		{
+		}).fail(function(data) {
+
 			// Clear timeouts if there are any
 			if(hangTO) clearTimeout(hangTO);
 			if(cancelTO) clearTimeout(cancelTO);
@@ -73,19 +69,16 @@ var Api = function()
 	
 	// PUBLIC FUNCTIONS -------------------------------------------------------
 
-	// call()
-	//
-	// Make a call to a route
-	//
-	// @route	String value from the API.routes object
-	// @args	Object with following key options:
-	//
-	//			@done			Callback fct, called when a result is returned
-	//			@fail			Callback fct, called on route failure
-	//			@method			Defaults to 'POST'
-	//			@cancellable	Enfore timelimits to call, defaults to false
-	//			@values			The data object that you want to pass along
-
+	/**
+	 * Make a call to a route.
+	 * @param  {String} route String value from the API.routes object.
+	 * @param  {Object} args Function options
+	 * @param  {Object} args.values The data object that you want to pass to the call
+	 * @param  {Function} [args.done] Callback fct, called when a result is returned.
+	 * @param  {Function} [args.fail] Callback fct, called on route failure.
+	 * @param  {String} [args.method='POST'] Method of the call.
+	 * @param  {Boolean} [args.cancellable=false] Enforce timelimits to call, defaults to false
+	 */
 	function call(route, args)
 	{
 		// Extend default settings object
