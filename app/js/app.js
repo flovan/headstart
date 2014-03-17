3483a548a142beff6d55cb63f808325c00f5e69a
Florian <florian@adagioagency.com>
2014-03-17 14:09:10 +0100

    Updates
    Cleaned up, better js structure, dropped oldie IE support, disabled
    image optimization because it was not working. Added TODO’s

--- app/js/app.js
+++ app/js/app.js
@@ -12,9 +12,7 @@ var App = (function() {
 
 	// VARS -------------------------------------------------------------------
 	
-	var	config				= null,
-		api					= null,
-		view				= null,
+	var	view				= null,
 		dispatcher			= $({});
 	
 	// CONSTRUCTOR ------------------------------------------------------------
@@ -23,46 +21,15 @@ var App = (function() {
 
 		log('_## Application initiated_');
 
-		// Patch IE if necessary
-		// Pass false for development mode, true for production mode
-		if(Utils.isOldie()) patchIE(false);
-
-		// Instantiate Core files
-		config = new Config();
-		api = new Api();
-
 		// Instantiate View class that is passed through init
 		view = view.init();
 	};
-
-	// PRIVATE FUNCTIONS ------------------------------------------------------
-
-	function patchIE() {
-
-		// Load and include the js patches
-		var script = document.createElement('script');
-		script.onreadystatechange = function() {
-
-			if (script.readyState === 'loaded') {
-				document.body.appendChild(script);
-			}
-		};
-		script.src = 'js/ie' + (isProd ? '.min' : '') + '.js';
-
-		// Load and include the css patches
-		if (document.createStyleSheet) {
-			document.createStyleSheet('css/ie' + (isProd ? '.min' : '') + '.css');
-		} else {
-			$('<link rel="stylesheet" type="text/css" href="css/ie' + (isProd ? '.min' : '') + '.css" />').appendTo('head');
-		}
-	}
 	
 	// PUBLIC FUNCTIONS -------------------------------------------------------
 	
 	return {
 		init: init,
-		config: config,
-		api: api,
+		view: view,
 		dispatcher: dispatcher,
 	};
 }());
