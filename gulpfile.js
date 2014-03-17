4ce4547c6a7aac6958f4d83113c5a9a4ddf1f524
Florian <accounts@flovan.me>
2014-03-17 14:12:04 +0100

    Merge branch 'wip'

--- gulpfile.js
+++ gulpfile.js
@@ -141,11 +141,8 @@ gulp.task('sass', function()
 //
 // Making sure your scripts are squeaky-clean, settings in '.jshintrc' file
 //
-// Note:	Before running production it is advised to insert following option:
-//			"undef": true
-//
-//			It is not included by default because it also throws errors on
-//			global variables such as "window" and "document".
+// JSHint options:	http://www.jshint.com/docs/options/
+// HTMLHint option:	https://github.com/yaniswang/HTMLHint/wiki/Rules
 
 gulp.task('hint-html', function(cb)
 {
@@ -155,22 +152,13 @@ gulp.task('hint-html', function(cb)
 		.pipe(htmlhint.reporter(stylish));
 });
 
-gulp.task('hint-main', function(cb)
+gulp.task('hint-script', function(cb)
 {
 	if(!config.hint) cb(null);
 	else return gulp.src([
-				config.app + '/js/app.js'
+				config.app + '/js/*.js'
 			,	config.app + '/js/core/*.js'
-			,	'!' + config.app + '/js/**/log.js'
 		])
-		.pipe(jshint('.jshintrc'))
-		.pipe(jshint.reporter(stylish));
-});
-
-gulp.task('hint-view', function(cb)
-{
-	if(!config.hint) cb(null);
-	else return gulp.src(config.app + '/js/view-*.js')
 		.pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter(stylish));
 });
@@ -179,48 +167,38 @@ gulp.task('hint-view', function(cb)
 //
 // Concatinating and uglifying
 // Also strips out all references to console.log() or log()
-//
-// JSHint options:	http://www.jshint.com/docs/options/
-// HTMLHint option:	https://github.com/yaniswang/HTMLHint/wiki/Rules
 
-gulp.task('scripts-main', ['hint-main'], function()
+gulp.task('scripts-main', ['hint-script'], function()
 {
 	return gulp.src([
 				config.app + '/js/libs/jquery*.js'
+
+			,	(isProduction ? '!' : '') + config.app + '/js/libs/dev/*.js'
+
+			,	config.app + '/js/libs/patches/matchmedia.polyfill.js'
+			,	config.app + '/js/libs/patches/matchmedia.addListener.js'
+			,	config.app + '/js/libs/patches/respond.js'
+			,	config.app + '/js/libs/patches/*.js'
+
 			,	config.app + '/js/libs/*.js'
-			,	(isProduction ? '!**/log.js' : '')
 			,	config.app + '/js/core/*.js'
-			,	config.app + '/js/app.js'
+			,	config.app + '/js/*.js'
+			,	'!' + config.app + '/js/view-*.js'
+
+			,	'!**/_*.js'
 		], {base: './' + config.app + '/js'})
 		.pipe(gulpif(isProduction, concat('core-libs.min.js')))
-		.pipe(gulpif(isProduction, replace(/(console\.)?log(.*?);?/g, '')))
+		.pipe(gulpif(isProduction, replace(/(\/\/)?(console\.)?log\((.*?)\);?/g, '')))
 		.pipe(gulpif(isProduction, uglify()))
 		.pipe(gulp.dest(runDir + '/js'));
 });
 
-gulp.task('scripts-view', ['hint-view'], function()
+gulp.task('scripts-view', ['hint-script'], function()
 {
 	return gulp.src(config.app + '/js/view-*.js')
 		.pipe(gulpif(isProduction, rename({suffix: '.min'})))
-		.pipe(gulpif(isProduction, replace(/(console\.)?log(.*?);?/g, '')))
+		.pipe(gulpif(isProduction, replace(/(\/\/)?(console\.)?log\((.*?)\);?/g, '')))
 		.pipe(gulpif(isProduction, uglify()))
-		.pipe(gulp.dest(runDir + '/js'));
-});
-
-gulp.task('scripts-ie', function()
-{
-	return gulp.src([
-				config.app + '/js/libs/ie/html5shiv.js'
-			,	config.app + '/js/libs/ie/PIE.js'
-			,	config.app + '/js/libs/ie/nwmatcher-1.2.5.js'
-			,	config.app + '/js/libs/ie/selectivizr.js'
-			,	config.app + '/js/libs/ie/matchmedia.polyfill.js'
-			,	config.app + '/js/libs/ie/matchmedia.addListener.js'
-			,	config.app + '/js/libs/ie/respond.js'
-			,	config.app + '/js/libs/ie/*.js'
-		])
-		.pipe(concat('ie.min.js'))
-		.pipe(uglify())
 		.pipe(gulp.dest(runDir + '/js'));
 });
 
@@ -236,7 +214,7 @@ gulp.task('images', function(cb)
 			,	config.app + '/images/**/*.gif'
 		])
 		.pipe(newer(runDir + '/images'))
-		.pipe(gulpif(isProduction, cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))))
+		//.pipe(gulpif(isProduction, cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))))
 		.pipe(gulp.dest(runDir + '/images'))
 		.pipe(gulpif(lrStarted && config.useLR, connect.reload()));
 
@@ -299,10 +277,20 @@ gulp.task('html', ['hint-html'], function(cb)
 			// Development gets raw base files
 			var injectItems = isProduction ? [config.dist + '/js/core-libs.min.js'] : [
 						config.app + '/js/libs/jquery*.js'
-					,	config.app + '/js/libs/*.js'
-					,	(isProduction ? '!**/log.js' : '')
-					,	config.app + '/js/core/*.js'
-					,	config.app + '/js/app.js'
+
+				,	(isProduction ? '!' : '') + config.app + '/js/libs/dev/*.js'
+
+				,	config.app + '/js/libs/patches/matchmedia.polyfill.js'
+				,	config.app + '/js/libs/patches/matchmedia.addListener.js'
+				,	config.app + '/js/libs/patches/respond.js'
+				,	config.app + '/js/libs/patches/*.js'
+
+				,	config.app + '/js/libs/*.js'
+				,	config.app + '/js/core/*.js'
+				,	config.app + '/js/*.js'
+				,	'!' + config.app + '/js/view-*.js'
+
+				,	'!**/_*.js'
 				];
 
 			// Add specific js and css files
@@ -367,16 +355,11 @@ gulp.task('server', function()
 
 	// JS specific watches to also detect removing/adding of files
 	// Note: Will also run the HTML task again (when needed) to update the linked files
-	watch({ glob: config.app + '/js/**/ie/*.js', emitOnGlob: false, name: 'JS-IE', emit: 'all' }, function() {
-		gulp.start('scripts-ie');
-	}).pipe(plumber())
-		.pipe(gulpif(lrStarted && config.useLR, connect.reload()));
-
 	watch({ glob: [config.app + '/**/view-*.js'], emitOnGlob: false, name: 'JS-VIEW', emit: 'all' }, function() {
 		sequence('scripts-view', 'html');
 	}).pipe(plumber());
 
-	watch({ glob: [config.app + '/js/**/*.js', '!**/ie/*.js', '!**/view-*.js'], emitOnGlob: false, name: 'JS', emit: 'all' }, function() {
+	watch({ glob: [config.app + '/js/**/*.js', '!**/view-*.js'], emitOnGlob: false, name: 'JS-MAIN', emit: 'all' }, function() {
 		sequence('scripts-main', 'html');
 	}).pipe(plumber());
 
@@ -426,7 +409,7 @@ gulp.task('zip', function(cb)
 
 gulp.task('default', function()
 {
-	sequence('clean', ['sass', 'scripts-view', 'scripts-main', 'scripts-ie', 'fonts', 'images', 'misc'], 'html', function()
+	sequence('clean', ['sass', 'scripts-view', 'scripts-main', 'fonts', 'images', 'misc'], 'html', function()
 	{
 		if(!isProduction) gulp.start('server');
 		else gulp.start('zip');
