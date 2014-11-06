0d1d9c72165ca2b3365bd3bf6380f6335564a08b
Florian <accounts@flovan.me>
2014-11-06 22:57:20 +0100

    More extracting, settings module
    wip

--- gulpfile.js
+++ gulpfile.js
@@ -9,39 +9,14 @@ var
 	ncp                 = require('ncp').ncp,
 	chalk               = require('chalk'),
 	ProgressBar         = require('progress'),
-	flags               = require('minimist')(process.argv.slice(2)),
 	utils               = require('./lib/utils')
 ;
 
-// VARS -----------------------------------------------------------------------
+// GLOBAL SETTINGS ------------------------------------------------------------
+//
+// This object gets shared with the task partials
 
-var
-	cwd                 = process.cwd(),
-	tmpFolder           = '.tmp',
-	stdoutBuffer        = [],
-	lrStarted           = false,
-	htmlminOptions      = {
-		removeComments:                true,
-		collapseWhitespace:            true,
-		collapseBooleanAttributes:     true,
-		removeAttributeQuotes:         true,
-		useShortDoctype:               true,
-		removeScriptTypeAttributes:    true,
-		removeStyleLinkTypeAttributes: true,
-		minifyJS:                      true,
-		minifyCSS:                     true
-	},
-	isProduction        = ( flags.production || flags.p ) || false,
-	isServe             = ( flags.serve || flags.s ) || false,
-	isOpen              = ( flags.open || flags.o ) || false,
-	isEdit              = ( flags.edit || flags.e ) || false,
-	isVerbose           = flags.verbose || false,
-	isTunnel            = ( flags.tunnel || flags.t ) || false,
-	tunnelUrl           = null,
-	isPSI               = flags.psi || false,
-	config              = null,
-	bar                 = null
-;
+require('./lib/settings');
 
 // LOGGING --------------------------------------------------------------------
 //
