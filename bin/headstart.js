00d6265ad28dc393a5bece88a7ae2b5f7015bf48
Florian <accounts@flovan.me>
2015-03-24 08:10:50 +0100

    More refactoring, WIP config cmd

--- bin/headstart.js
+++ bin/headstart.js
@@ -3,89 +3,103 @@
 
 // Require modules
 
-var 
-	path                = require('path'),
-	fs                  = require('fs'),
-	chalk               = require('chalk'),
-	updateNotifier      = require('update-notifier'),
-	argv                = require('minimist')(process.argv.slice(2)),
-
-	pkg                 = require('../package.json'),
-	cli                 = require('../lib/cli'),
-	settings			= require('../lib/settings')
+var
+	deps        = require('../lib/dependencies'),
+	pkg         = require('../package.json'),
+	cli         = require('../lib/cli'),
+	settings	= require('../lib/settings'),
+	utils       = require('../lib/utils')
 ;
 
 // Define variables
 
 var
-	notifier,
-	versionFlag         = argv.v || argv.version,
-	infoFlag            = argv.i || argv.info || argv.h || argv.help,
+	c           = deps.chalk,
+	argv        = deps.minimist(process.argv.slice(2)),
+	versionFlag = argv.v || argv.version,
+	infoFlag    = argv.i || argv.info || argv.h || argv.help,
 
-	task                = argv._,
-	numTasks            = task.length
+	task        = argv._,
+	taskDir     = deps.path.join(__dirname, '../lib/tasks'),
+
+	notifier, spacer, current, latest, instructions, link
 ;
 
-// Check for updates
+// Define functions
 
-notifier = updateNotifier({
-	packageName:    pkg.name,
-	packageVersion: pkg.version
-}).notify();
+function cleanTask (task) {
+	// Make sure we only have one task
+	if (task.length > 1) {
+		console.log(c.red('\nOnly one task can be provided. Aborting.\n'));
+		cli.logTasks();
+		process.exit(0);
+	}
 
-if (notifier.update) {
-	console.log('Instructions can be found on: http://headstart.io/upgrading-guide');
+	// Grab that task
+	task = task[0];
+
+	// Check if it is valid by comparing it the the CLI
+	if (cli.tasks.indexOf(task) < 0) {
+		console.log(c.red('\nThe provided task "' + task + '" was not recognized. Aborting.\n'));
+		cli.logTasks();
+		process.exit(0);
+	}
+
+	return task;
 }
 
-// Check for version flag
+function checkUpdates () {
+	notifier = deps.updateNotifier({pkg: pkg, updateCheckInterval: 0});
+	if (notifier.update) {
 
-if (versionFlag) {
-	cli.logHeader(pkg);
-	process.exit(0);
+		spacer = c.yellow('| '),
+		current = c.gray('(current: ' + notifier.update.current + ')'),
+		latest = c.green(notifier.update.latest),
+		instructions = c.cyan('(sudo) npm update -g headstart'),
+		link = c.cyan.underline('http://headstart.io/upgrade')
+
+		console.log('\n' + [
+			spacer + 'New version available: ' + latest + ' ' + current,
+			spacer,
+			spacer + 'Run ' + instructions + ' to update',
+			spacer + 'Further instruction on: ' + link
+		].join('\n'));
+	}
 }
 
-// Print info if needed or if no tasks are passed in
+function checkFlags () {
+	// Check for version flag
+	if (versionFlag) {
+		cli.logHeader(pkg);
+		process.exit(0);
+	}
 
-if(infoFlag || !numTasks) {
-	cli.logHeader(pkg);
-	cli.logTasks();
-	process.exit(0);
+	// Print info if needed or if no tasks are passed in
+	if(infoFlag || !task.length) {
+		cli.logHeader(pkg);
+		cli.logTasks();
+		process.exit(0);
+	}
 }
 
-// Warn if more than one tasks has been passed in
+function init () {
+	// Clean the passed in task
+	task = cleanTask(task);
 
-if (numTasks > 1) {
-	console.log(chalk.red('\nOnly one task can be provided. Aborting.\n'));
-	cli.logTasks();
-	process.exit(0);
+	// Register all tasks
+	require(taskDir);
+
+	// If there are no global settings and we are not configuring,
+	// run the config task. Otherwise, just run the task
+	if (task !== 'config' && !settings.global.size) {
+		deps.taker.series('config', task);
+	} else {
+		deps.taker.parallel(task)();
+	}
 }
 
-// We are now sure we only have 1 task
+// Execute logic
 
-task = task[0];
-
-// Check if task is valid
-
-if (cli.tasks.indexOf(task) < 0) {
-	console.log(chalk.red('\nThe provided task "' + task + '" was not recognized. Aborting.\n'));
-	cli.logTasks();
-	process.exit(0);
-}
-
-// Register Gulp tasks through external files
-
-fs.readdirSync(path.join(__dirname, '../lib/tasks')).forEach(function(file) {
-	require('../lib/tasks/' + file);
-});
-
-// Patch the output when not running verbose mode
-
-if (!settings.isVerbose) {
-	cli.patchOutput();
-}
-
-// Start the task through Gulp
-
-process.nextTick(function () {
-	require('gulp').parallel(task)();
-});
+checkUpdates();
+checkFlags();
+init();
