138d98674577b0ebb122a0cf0fd3d2aa3ba705dc
Florian <accounts@flovan.me>
2015-09-13 22:49:33 +0200

    WIP reiterating, simplifying

--- bin/headstart.js
+++ bin/headstart.js
@@ -1,109 +1,61 @@
 #!/usr/bin/env node
 'use strict';
 
-// Require modules
+// PHASE 1
+// Put basic CLI options on the fast track
 
 var
-	deps        = require('../lib/dependencies'),
 	pkg         = require('../package.json'),
 	cli         = require('../lib/cli'),
+	deps        = require('../lib/dependencies'),
 	settings	= require('../lib/settings'),
-	utils       = require('../lib/utils')
+	utils       = require('../lib/utils'),
+
+	argv        = deps.minimist(process.argv.slice(2)),
+	task        = argv._[0],
+	versionFlag = argv.v || argv.version,
+	infoFlag    = argv.i || argv.info || argv.h || argv.help || task === 'help' || task === 'info'
 ;
 
-// Define variables
+// Check for version flag
+if (versionFlag) {
+	console.log(pkg.version);
+	process.exit(1);
+}
+
+// Print info if needed or if no tasks are passed in
+if(infoFlag || !task) {
+	cli.logTasks(pkg.version);
+	process.exit(1);
+}
+
+// PHASE 2
+// Check for updates and handle non-basic input and run tasks.
 
 var
 	c           = deps.chalk,
-	argv        = deps.minimist(process.argv.slice(2)),
-	versionFlag = argv.v || argv.version,
-	infoFlag    = argv.i || argv.info || argv.h || argv.help,
-
-	task        = argv._,
 	taskDir     = deps.path.join(__dirname, '../lib/tasks'),
-
-	notifier,
-	spacer,
-	current,
-	latest,
-	instructions,
-	link
+	notifier    = deps.updateNotifier({pkg: pkg, updateCheckInterval: 0})
 ;
 
-// Define functions
-
-function cleanTask (task) {
-	// Make sure we only have one task
-	if (task.length > 1) {
-		console.log(c.red('\nOnly one task can be provided. Aborting.\n'));
-		cli.logTasks();
-		process.exit(0);
-	}
-
-	// Grab that task
-	task = task[0];
-
-	// Check if it is valid by comparing it the the CLI
-	if (cli.tasks.indexOf(task) < 0) {
-		console.log(c.red('\nThe provided task "' + task + '" was not recognized. Aborting.\n'));
-		cli.logTasks();
-		process.exit(0);
-	}
-
-	return task;
+// Check for updates
+if (notifier.update) {
+	notifier.notify();
 }
 
-function checkUpdates () {
-	notifier = deps.updateNotifier({pkg: pkg, updateCheckInterval: 0});
-	if (notifier.update) {
-		spacer = c.yellow('| '),
-		current = c.gray('(current: ' + notifier.update.current + ')'),
-		latest = c.green(notifier.update.latest),
-		instructions = c.cyan('(sudo) npm update -g headstart'),
-		link = c.cyan.underline('http://headstart.io/upgrade')
-
-		console.log('\n' + [
-			spacer + 'New version available: ' + latest + ' ' + current,
-			spacer,
-			spacer + 'Run ' + instructions + ' to update',
-			spacer + 'Further instruction on: ' + link
-		].join('\n'));
-	}
+// Check if it is valid by comparing it the the CLI
+if (cli.tasks.indexOf(task) < 0) {
+	console.log(c.red('\nThe provided task "' + task + '" was not recognized. Aborting.\n'));
+	process.exit(1);
 }
 
-function checkFlags () {
-	// Check for version flag
-	if (versionFlag) {
-		cli.logHeader(pkg);
-		process.exit(0);
-	}
+// Register all tasks
+require(taskDir);
 
-	// Print info if needed or if no tasks are passed in
-	if(infoFlag || !task.length) {
-		cli.logHeader(pkg);
-		cli.logTasks();
-		process.exit(0);
-	}
+// If there are no global settings and we are not configuring,
+// run the config task. Otherwise, just run the task
+if (task !== 'config' && !settings.global.size) {
+	deps.taker.series('config', task);
+} else {
+	deps.taker.parallel(task)();
 }
-
-function init () {
-	// Clean the passed in task
-	task = cleanTask(task);
-
-	// Register all tasks
-	require(taskDir);
-
-	// If there are no global settings and we are not configuring,
-	// run the config task. Otherwise, just run the task
-	if (task !== 'config' && !settings.global.size) {
-		deps.taker.series('config', task);
-	} else {
-		deps.taker.parallel(task)();
-	}
-}
-
-// Execute logic
-
-checkUpdates();
-checkFlags();
-init();
---
+++ docs/assets.md
@@ -0,0 +1,8 @@
+Autoprefix defaults:
+```
+['> 1%', 'last 2 versions', 'Firefox 28', 'Opera 12.1', 'ie 9']
+```
+
+- FF 28: that version still needs -moz to support border-box
+- Opera 12.1: the last version to have the Presto render engine before they switched to Webkit
+- IE9: the oldest IE browser that makes any sense to support
